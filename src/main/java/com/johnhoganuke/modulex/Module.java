b1b9f420e5a382602b5d55deb76a323dd3441572
John Kayne <johnhoganuke2@gmail.com>
2018-07-12 19:39:09 -0400

    Update Module.java

--- src/main/java/com/johnhoganuke/modulex/Module.java
+++ src/main/java/com/johnhoganuke/modulex/Module.java
@@ -12,8 +12,17 @@ public interface Module<M extends Modular> {
   
   /**
    * allows the module to change or alter the modular object apon being added to the module.
+   * 
    * @param m The modular object that the module is being added to.
    */
   public void addTo(M m);
+  
+  /**
+   * allows the module to revoke changes that were made to the modular object apon the module
+   * being added to the modular object (<code>m</code>).
+   * 
+   * @param m The modular object that the module is being removed from.
+   */
+  public void removeFrom(M m);
   
 }
