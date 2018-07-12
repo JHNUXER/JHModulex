2c51e8830e40905bbba338dbd65827df66c1409a
John Kayne <johnhoganuke2@gmail.com>
2018-07-12 19:34:03 -0400

    Create Modular.java

---
+++ src/main/java/com/johnhoganuke/modulex/Modular.java
@@ -0,0 +1,16 @@
+package com.johnhoganuke.modulex;
+
+/**
+ * The basic functionality required for a modular object.
+ */
+public interface Modular<M extends Module> {
+
+  /**
+   * Add a module to this modular object. (Note: This method has a default definition which calls <code>addTo(this)</code>
+   * on the module.
+   * 
+   * @param m the module to add to this modular object.
+   */
+  public default void addModule(M m) { m.addTo(this); }
+  
+}
