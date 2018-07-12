ae70fdbb3d84227b793881bdf3747c07fd87f9c3
John Kayne <johnhoganuke2@gmail.com>
2018-07-12 19:40:02 -0400

    Update Modular.java

--- src/main/java/com/johnhoganuke/modulex/Modular.java
+++ src/main/java/com/johnhoganuke/modulex/Modular.java
@@ -13,4 +13,12 @@ public interface Modular<M extends Module> {
    */
   public default void addModule(M m) { m.addTo(this); }
   
+  /**
+   * Remove a module from this modular object. (Note: This method has a default definition which calls <code>removeFrom(this)</code>
+   * on the module.
+   * 
+   * @param m the module to add to this modular object.
+   */
+  public default void addModule(M m) { m.addTo(this); }
+  
 }
